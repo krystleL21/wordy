4254e43da177d540c2ccdc433dd39ebacd6c01de
krystleL21 <54385637+krystleL21@users.noreply.github.com>
2025-08-30 09:55:44 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import kluxlogo from './kluxlogo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={kluxlogo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
